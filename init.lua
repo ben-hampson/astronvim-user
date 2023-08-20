5abba65e25fa30a82bd6d9b3513ae33fff4478ab
Ben Hampson <email@email.com>
2023-08-20 23:56:53 +0100

    remove comments

--- init.lua
+++ init.lua
@@ -99,18 +99,6 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
     -- Run dap-ui when a debugging session is started
     local dap = require("dap")
     local dapui = require("dapui")
