7823d478f439f64c0ad1459cd0abbd1482ad92a9
Ben Hampson <email@email.com>
2023-08-22 19:43:37 +0100

    add config options for neotest-python

--- plugins/neotest.lua
+++ plugins/neotest.lua
@@ -4,6 +4,7 @@ return {
     "nvim-neotest/neotest-python"
   },
   {
+    -- https://github.com/nvim-neotest/neotest-python
     "nvim-neotest/neotest",
     requires = {
       "nvim-lua/plenary.nvim",
@@ -13,20 +14,14 @@ return {
     opts = function()
       return {
         adapters = {
-          require "neotest-python"
-          --   ({
-          --   -- neotest-python config
-          --   -- Extra arguments for nvim-dap configuration
-          --   -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-          --   dap = { justMyCode = false },
-          --   runner = "pytest",
-          --   -- Command line arguments for runner
-          --   -- Can also be a function to return dynamic values
-          --   args = {"--log-level", "DEBUG"},
-          -- }
-          -- )
-        }
+          -- https://github.com/nvim-neotest/neotest-python
+          require("neotest-python")({
+            dap = { justMyCode = false },
+            args = {"--log-level", "DEBUG"},
+            runner = "pytest",
+          }),
+        },
       }
-      end,
+    end,
   }
 }
