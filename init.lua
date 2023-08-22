de96f3bf20534913069a65a46e532aac546dcab5
Ben Hampson <email@email.com>
2023-08-22 23:49:54 +0100

    set colorscheme to cattpuccin

--- init.lua
+++ init.lua
@@ -15,9 +15,6 @@ return {
     },
   },
 
-  -- Set colorscheme to use
-  colorscheme = "astrodark",
-
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
